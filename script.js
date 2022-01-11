bd7e74388b49580d8a31b0d9bf7ba619580fc20d
gintsauzins <devozyag@gmail.com>
2022-01-11 18:36:19 +0200

    before add photos

--- script.js
+++ script.js
@@ -8,6 +8,33 @@ let menu = document.querySelector('.dropdown_link');
 menu.onclick = function (event) {
     event.preventDefault();
     document.querySelector('.top_menu').classList.toggle('active');
-
 }
 
+
+
+//--------------- Šie kodi uztaisa, to ka uzspiezot uz post vai tagged, iezimejas tas uz kuru uzspiez un otrs izlsedzas automatiski------
+
+const gallery_nav = document.querySelector('.gallery_nav');
+
+
+//querySelector atgirez vienu elementu querySelectorAll var 2 un vairak
+gallery_nav.children[0].onclick = function(event) {
+    let active_link = gallery_nav.querySelector('.active');
+    if (active_link) {
+        active_link.classList.remove('active');
+    }
+
+   this.classList.add('active');
+}
+
+//querySelector atgirez vienu elementu querySelectorAll var 2 un vairak
+gallery_nav.children[1].onclick = function(event) {
+    let active_link = gallery_nav.querySelector('.active');
+    if (active_link) {
+        active_link.classList.remove('active');
+    }
+
+    this.classList.add('active');
+  }
+  
+//-------------------------------------------------------
